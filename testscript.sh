61dac41b73bb1f70b7aa429ba8370d07bad2dd83
cgc465 <78361418+cgc465@users.noreply.github.com>
2023-04-14 16:47:35 -0400

    Update testscript.sh
    committing again at 4:47

--- testscript.sh
+++ testscript.sh
@@ -2,5 +2,5 @@
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
-echo "Edited this file today 4/14/2023 @4:37pm"
+echo "Edited this file today 4/14/2023 @4:47pm"
 echo "This was added"
